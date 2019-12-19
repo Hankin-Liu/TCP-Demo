bcb5e1e1b05183b53112eab9f303ba398eaa90f7
Hankin-Liu <wojiaoliuhuajun@126.com>
2019-12-19 11:48:09 +0800

    Update test.h
    add comments for test.h

--- tcp_server/src/test.h
+++ tcp_server/src/test.h
@@ -6,8 +6,9 @@
 #include "common/pkg_def.h"
 
 using namespace actor;
-
+//1字节对齐
 #pragma pack(push, 1)
+//数据包定义
 struct pkg_test
 {
     DEF_FUNC_CODE(99999)
@@ -17,9 +18,12 @@ struct pkg_test
 };
 #pragma pack(pop)
 
+//业务类,需要继承actor库的actor_framework类
 class test_class : public actor_framework
 {
     public:
+        //重写虚函数,注册回调函数
         virtual int32_t pre_action() override;
+        //重写虚函数,暂时没用,后续扩展用
         virtual int32_t post_action() override;
 };
